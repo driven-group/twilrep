2572976d5d28b2f8a922362bcd5ffc5fa784a488
driven-group <marketing@drivenautogroupcom.au>
2023-08-08 13:26:33 +0800

    added sb test

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MessagingResponse } = require('twilio').twiml;
+const { createClient } = require('@supabase/supabase-js')
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+const logReply = async (req) => {
+  const { error } = await supabase
+        .from('sms')
+        .insert({
+          from: req.body.From,
+          message: req.body.Body
+        })
+        .then((response) => {
+            console.log(response.status)
+            return response.status
+        })
+        .catch((error) => {
+            console.log(error)
+            return error
+        })
+}
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
-  console.log(req.body)
+  logReply(req)
   const twiml = new MessagingResponse();
 
   twiml.message('Thank you for your message. This number is automated and will not reply, please call directly: \n+61861866777\nOr contact us via our website with any queries.\nhttps://yourcarsold.com.au');
